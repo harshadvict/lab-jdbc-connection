e5724cf8509b8416750adf81038b5fac7603ad75
harshadvict <harshadvict@gmail.com>
2020-09-22 22:38:04 +0530

    harsh

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,20 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		
-	// Fill your code
+		ConnectionManager obj=new ConnectionManager();
+		Connection obj2=obj.getConnection();
+		if(obj2!=null) {
+			System.out.println("Connection established");
+		}
+		else
+			System.out.println("Check your connection");
 		
 	}
 }
